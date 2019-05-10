113cecb8584173acabe0a679a45ea84b34ce425f
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 12:07:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,54 +1,93 @@
-def sort_array_asc(array)
-array.sort
-end
-
-def sort_array_desc(array)
-  array.sort do |left, right| right<=>left
-end
-end
-
-def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
-end
-end
-
-def swap_elements(array)
-  array[0], array[1], array[2] = array[0], array[2], array[1]
-  array
-end
-
-def reverse_array(array)
-  array[0], array[1], array[2] = array[2], array[1], array[0]
-  array
-end
-
-def kesha_maker(array)
-  array.each do |letter|
-    letter[2] = "$"
+def begins_with_r(array)
+  array.each do |it|
+    if !it.to_s.start_with?("r")
+      return false
+    end
   end
+  return true 
 end
 
-def find_a(array)
-  array.find_all do |word|
-    word[0] == "a"
-end
-end
 
-def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum += num
+def contain_a(array)
+  a_array = []
+  array.each do |thing|
+    if thing.include?("a"||"A")
+      a_array << thing
+    end
   end
-  sum
+  a_array
 end
 
-def add_s(array)
-  array.collect do |word|
- if array[1] == word
-   word
-else
-  word + "s"
+def first_wa(array)
+  first_wa_word = ""
+  array.find do |it|
+    if it[0..1] == "wa"
+      first_wa_word = it
+    end
+  end
+  first_wa_word
 end
+
+def remove_non_strings(array)
+  new_array = []
+  array.each do |thing|
+    if thing.is_a?(String)
+      new_array << thing
+    end
+  end
+  new_array
 end
+
+def count_elements(data)
+  new_hash = {}
+  data.each do |thing|
+    if new_hash.has_key?(thing[:name])
+      new_hash[thing[:name]] += 1 
+    else
+      new_hash[thing[:name]] = 1 
+    end
+  end
+  new_hash.map { |key, value| {:name => key , :count => value} }
+end
+
+def merge_data(keys, data)
+  new_array = []
+
+  keys.each do |thing|
+    thing.each do |lable, value|
+      data.each do |nested_thing|
+        nested_thing.each do |key,hash|
+          if value == key 
+            new_array << thing.merge(hash)
+          end
+        end
+      end
+    end
+  end
+   new_array   
+end
+
+def find_cool(array)
+  new_array = []
+  array.each do |thing|
+    if thing[:temperature] == "cool"
+      new_array << thing
+    end
+  end
+  new_array
+end
+
+def organize_schools(array)
+  schools_org = {}
+
+  array.each do |name, details|
+    location = details[:location]
+    if !schools_org[location]
+      schools_org[location] = []
+    end
+    if !schools_org[location].include?(name)
+      schools_org[location]<< name
+    end
+  end
+  schools_org
 end 
